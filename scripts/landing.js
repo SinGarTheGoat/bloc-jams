d18766e0593083300258c615fefcaef61d225a6f
Forrest Stone <iloveburningman@gmail.com>
2016-10-02 13:10:23 -0700

    Chapter 22 HW done

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,18 +1,19 @@
 var animatePoints = function() {
  
-     var points = document.getElementsByClassName('point');
+    var points = document.getElementsByClassName('point');
  
-    var revealPoint = function(points){
-        (i=0;i<points.length;i++){
+    var revealPoint = function(i){
          points[i].style.opacity = 1;
          points[i].style.transform = "scaleX(1) translateY(0)";
          points[i].style.msTransform = "scaleX(1) translateY(0)";
          points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
         
-        }
+        
     }
-    revealPoint();
+    for (var i=0;i<points.length;i++){
+        revealPoint(i);
+    }
+   
     
- 
  };
 animatePoints()
