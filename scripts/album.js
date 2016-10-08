e69308f3d144151faec156c2bc3105827582abbe
Forrest Stone <iloveburningman@gmail.com>
2016-10-07 17:56:49 -0700

    Chapter 26 follow along done

--- scripts/album.js
+++ scripts/album.js
@@ -53,7 +53,7 @@ var albumZevon = {
 var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
-      + '  <td class="song-item-number">' + songNumber + '</td>'
+       + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
       + '  <td class="song-item-title">' + songName + '</td>'
       + '  <td class="song-item-duration">' + songLength + '</td>'
       + '</tr>'
@@ -85,8 +85,33 @@ var createSongRow = function(songNumber, songName, songLength) {
  
 var koolKids = [albumPicasso, albumMarconi, albumZevon]
 var koolKidsKounter = 0;
+
+
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
+ // Album button templates
+ var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+
+
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
+     
+     
+     songListContainer.addEventListener('mouseover', function(event) {
+         // Only target individual song rows during event delegation
+         if (event.target.parentElement.className === 'album-view-song-item') {
+             event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+         }
+     });
+     
+     
+     for (var i = 0; i < songRows.length; i++) {
+         songRows[i].addEventListener('mouseleave', function(event) {
+              // Selects first child element, which is the song-item-number element
+             this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+         });
+     }
+     
      document.getElementById("albumImage").addEventListener('click', function() {
             koolKidsKounter++;
          if (koolKidsKounter>koolKids.length){
