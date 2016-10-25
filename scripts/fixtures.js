7c1d2882cb1358a476449e25986d9c519e905642
Forrest Stone <iloveburningman@gmail.com>
2016-10-24 21:42:08 -0700

    Before the ParseInt()

---
+++ scripts/fixtures.js
@@ -0,0 +1,49 @@
+ // Example Album
+
+ var albumPicasso = {
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { title: 'Blue', duration: '4:26' },
+         { title: 'Green', duration: '3:14' },
+         { title: 'Red', duration: '5:01' },
+         { title: 'Pink', duration: '3:21'},
+         { title: 'Magenta', duration: '2:15'}
+     ]
+ };
+
+var albumZevon = {
+     title: 'Bad Luck Streak In Dancing School',
+     artist: 'Warren Zevon',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/Warren.jpg',
+     songs: [
+         { title: '"Bad Luck Streak in Dancing School"  ', duration: '4:26' },
+         { title: 'A Certain Girl', duration: '3:14' },
+         { title: 'Empty-Handed Heart', duration: '5:01' },
+         { title: 'Interlude No. 1', duration: '3:21'},
+         { title: 'Play It All Night Long', duration: '2:15'}
+     ]
+    
+}
+ 
+ // Another Example Album
+ var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21'},
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15'}
+     ]
+ };
+
